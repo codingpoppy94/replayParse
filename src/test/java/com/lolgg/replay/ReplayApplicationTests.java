3bd663241e7dc089a3ad88d007ca682685390e4d
codingpoppy94 <60597797+codingpoppy94@users.noreply.github.com>
2024-05-28 14:21:04 +0900

    init

---
+++ src/test/java/com/lolgg/replay/ReplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lolgg.replay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
